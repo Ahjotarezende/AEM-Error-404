e38a669bf2ad291b47e93c5bf58b8b6938d2a678
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-07-29 13:39:42 -0300

    feat: created and styled header component

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -21,6 +21,7 @@ import "./ExperienceFragment/ExperienceFragment";
 import "./XxXxx";
 import MeuComponente from "./MeuComponente";
 import "./TextComponent/Text"
+import "./HeaderComponent/Header"
 
 import { MapTo } from "@adobe/aem-react-editable-components";
 
